190d2822509eb64638c6cf1e256a3a8408596075
koki.chin <koki.chin@ixs.co.jp>
2024-05-24 16:24:02 +0900

    i

--- nvidia-smi.py
+++ nvidia-smi.py
@@ -1,15 +1,63 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 import subprocess
+import shlex
 
 app = FastAPI()
 
-@app.get("/nvidia-smi")
-async def get_nvidia_smi():
-    process = subprocess.Popen(
-        ["nvidia-smi", "-lms", "500"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout, stderr = process.communicate()
-    if process.returncode == 0:
-        return {"data": stdout.decode()}
-    else:
-        return {"error": stderr.decode()}
+
+from fastapi.staticfiles import StaticFiles
+
+# Mount the static directory to serve index.html
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/stream-nvidia-smi")
+def stream_nvidia_smi():
+    command = shlex.split("nvidia-smi -lms 500")
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
+
+    def generate():
+        buffer = ""
+        end_delimiter = "+-------------------------------+----------------------+----------------------+\n"
+        start_delimiter = "+-----------------------------------------------------------------------------+\n"
+        collecting = False  # This flag indicates if we are between the start and end delimiters
+
+        try:
+            # Read each line from the subprocess' stdout
+            for line in iter(process.stdout.readline, ''):
+                # print(buffer)
+                if start_delimiter in line:
+                    collecting = True  # Start collecting lines
+                    buffer = line  # Initialize buffer with the start delimiter line
+                elif collecting:
+                    buffer += line
+                    if end_delimiter in line:
+                        collecting = False  # Stop collecting lines
+                        # print(buffer)
+                        yield buffer.encode('utf-8')  # Send the complete block
+                        buffer = ""  # Reset buffer for the next block
+        finally:
+            process.stdout.close()
+            process.wait()
+
+    return StreamingResponse(generate(), media_type="text/plain")
+
+# @app.get("/stream-nvidia-smi")
+# def stream_nvidia_smi():
+#     # Prepare the command and split it correctly
+#     command = shlex.split("nvidia-smi -lms 500")
+
+#     # Start the subprocess with unbuffered output
+#     process = subprocess.Popen(
+#         command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1
+#     )
+
+#     def generate():
+#         # Output each line from the subprocess' stdout
+#         for line in iter(process.stdout.readline, ''):
+#             yield line.encode('utf-8')
+#         process.stdout.close()
+#         process.wait()
+
+#     return StreamingResponse(generate(), media_type="text/plain")
